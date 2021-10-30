02c9dbeb1082c315fe420aaea9bdf85362e20083
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-10-30 14:57:59 -0300

    Conhecendo o JSX

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 ReactDOM.render(
-    'Olá React', 
+    <div>
+        {/* strong coloca em negrito */}
+        <strong>Olá React!</strong> 
+    </div>,
     document.getElementById('root')
     );
