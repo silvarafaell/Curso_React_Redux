b3462fb321629daa55c87c0f12e6b38cc446f060
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-01 21:23:57 -0300

    React Fragment

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,14 +4,16 @@ import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div id="app">
         <Primeiro></Primeiro>
         <ComParametro 
         titulo="Situação do Aluno" 
-        aluno="Rafael" nota={10.0}>
-        </ComParametro>
+        aluno="Rafael" 
+        nota={10.0} />
+       <Fragmento />
     </div>,
     document.getElementById('root')
     );
