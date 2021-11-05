f7e727da20e5c576b4ab43d5a29be4d4e20bd5a4
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-05 20:44:34 -0300

    Componente Card #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Card from './components/layout/Card';
 import Aleatorio from './components/basicos/Aleatorio';
 import Fragmento from './components/basicos/Fragmento';
 import ComParametro from './components/basicos/ComParametro';
@@ -10,14 +11,26 @@ export default function App(props) {
     return (
         <div id="app">
         <h1>Fundamentos do React</h1>
-        <Aleatorio min={1} max={60} />
+
+        <Card titulo="#04 - Desafio Aleatorio">
+            <Aleatorio min={1} max={60} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
         <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parametro">
         <ComParametro 
         titulo="Situação do Aluno" 
         aluno="Rafael" 
         nota={10.0} 
         />
-       <Primeiro></Primeiro>
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+        </Card>
        </div>
     );
 }
