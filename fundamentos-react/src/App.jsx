0b5d07f8030090229d539b509e534e8bd542acab
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-06 15:00:17 -0300

    Componente Card #03

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -15,15 +15,15 @@ export default function App(props) {
 
         <div className="Cards">
 
-        <Card titulo="#04 - Desafio Aleatorio">
+        <Card titulo="#04 - Desafio Aleatorio" color="#FA6900">
             <Aleatorio min={1} max={60} />
         </Card>
 
-        <Card titulo="#03 - Fragmento">
+        <Card titulo="#03 - Fragmento" color="#E94C6F">
         <Fragmento />
         </Card>
 
-        <Card titulo="#02 - Com Parametro">
+        <Card titulo="#02 - Com Parametro" color="E8B71A">
         <ComParametro 
         titulo="Situação do Aluno" 
         aluno="Rafael" 
@@ -31,7 +31,7 @@ export default function App(props) {
         />
         </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
+        <Card titulo="#01 - Primeiro Componente" color="#588C73">
         <Primeiro></Primeiro>
         </Card>
         </div>
