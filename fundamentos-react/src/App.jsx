15184d063626c743ba159c11addc18938722f749
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-13 22:39:33 -0300

    Renderizacao Condicional #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react';
 
+import ParOuImpar from './components/condicional/ParOuImpar';
 import TabelaProdutos from './components/repeticao/TabelaProdutos';
 import ListaAlunos from './components/repeticao/ListaAlunos';
 import Familia from './components/basicos/Familia';
@@ -18,6 +19,10 @@ export default function App(props) {
             <h1>Fundamentos do React</h1>
 
             <div className="Cards">
+            <Card titulo="#08 - Renderização Condicional " color="#982395">
+                   <ParOuImpar numero={21}></ParOuImpar>
+                </Card>
+
             <Card titulo="#07 - Desafio Repetição " color="#3A9AD9">
                    <TabelaProdutos></TabelaProdutos>
                 </Card>
