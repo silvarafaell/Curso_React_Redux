8d5831000bd2b1ed2c6ef32506d115fb72e51320
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-20 09:58:13 -0300

    Componente Estado

--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
-    let nome = ''
-    let idade = 0
-    let nerd = false
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam){
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
+    const [a,b] = [1, 2]
+    const [nome, setNome] = useState('?')
+    const [idade, setIdade] = useState(0)
+    const [nerd, setNerd] = useState(false)
+ 
+    function fornecerInformacoes(nome, idade, nerd){
+       setNome(nome)
+       setIdade(idade)
+       setNerd(nerd) 
     }
 
     return (
