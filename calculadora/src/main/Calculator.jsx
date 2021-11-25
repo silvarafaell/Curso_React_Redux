38c128c94cfee1a933933b1a8381978cc35d607f
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-11-25 15:37:41 -0300

    ajuste

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -46,6 +46,7 @@ export default class Calculator extends Component {
                 <Button label="0" click={this.addDigit} double />
                 <Button label="." click={this.addDigit} />
                 <Button label="=" click={this.setOperation} operation />
+
             </div>
         )
     }
