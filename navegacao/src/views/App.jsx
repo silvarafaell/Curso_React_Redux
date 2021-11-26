e7628e2f460925c34ab349b20f5164b84f4b4464
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-11-26 14:25:08 -0300

    Aplicando estilo - Navagacao com React Router

--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import React from 'react'
+import Menu from '../components/layout/Menu'
+import Context from '../components/layout/Context'
 
 const App = props => (
     <div className="App">
-        Componente App
+        <Menu />
+        <Context />
     </div>
 )
 
