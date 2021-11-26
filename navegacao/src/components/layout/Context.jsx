8d49ccd03569eebda2c7f5e034995f1b412bca2d
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-11-26 11:12:00 -0300

    Estrutura do Projeto - Navegacao com React Router

---
+++ navegacao/src/components/layout/Context.jsx
@@ -0,0 +1,10 @@
+import './Context.css'
+import React from 'react'
+
+const Context = props => (
+    <main className="Context">
+        Componente Context
+    </main>
+)
+
+export default Context
