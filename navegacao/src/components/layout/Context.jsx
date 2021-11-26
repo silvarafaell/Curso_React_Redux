24968a12d62ca524ab8ce5f3934b5550b0677b21
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-11-26 16:25:55 -0300

    Navegacao com Parametro-Navegacao com React Router

--- navegacao/src/components/layout/Context.jsx
+++ navegacao/src/components/layout/Context.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from '../../views/examples/Home'
 import About from '../../views/examples/About'
+import Param from '../../views/examples/Param'
 
 const Context = props => (
     <main className="Context">
         <Routes>
             <Route path="/" caseSensitive={false} element={<Home />} />
             <Route path="/about" caseSensitive={false} element={<About />} />
+            <Route path="/param/:id" caseSensitive={false} element={<Param />} />
         </Routes>
     </main>
 )
