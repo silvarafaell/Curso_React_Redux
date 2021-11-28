2f8d4968095a2687e506cee728eb4a053811d332
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-11-28 19:49:38 -0300

    Página não encontrada

--- navegacao/src/components/layout/Context.jsx
+++ navegacao/src/components/layout/Context.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../../views/examples/Home'
 import About from '../../views/examples/About'
 import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
 
 const Context = props => (
     <main className="Context">
@@ -12,6 +13,7 @@ const Context = props => (
             <Route exact path="/" caseSensitive={false} element={<Home />} />
             <Route path="/about" caseSensitive={false} element={<About />} />
             <Route path="/param/:id" caseSensitive={false} element={<Param />} />
+            <Route exact path="*" caseSensitive={false} element={<NotFound />} />
         </Routes>
     </main>
 )
