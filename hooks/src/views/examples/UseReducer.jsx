daa5b4850a27bb91c714fb35c1c1a140de4e034e
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-12-27 11:54:41 -0300

    Desafio useReducer - Resposta

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -15,6 +15,14 @@ function reducer(state, action) {
     switch (action.type) {
         case 'numberAdd2':
             return { ...state, number: state.number + 2 }
+         case 'numberMulti7':
+            return { ...state, number: state.number * 7 }
+        case 'numberDiv25':
+            return { ...state, number: state.number / 25 }
+          case 'numberInt':
+            return { ...state, number: parseInt(state.number) }
+        case 'numberAddN':
+            return { ...state, number: state.number + action.payload}
         case 'login':
             return { ...state, user:  {name : action.payload}}
         default:
@@ -42,7 +50,17 @@ const UseReducer = (props) => {
                     <button className="btn"
                     onClick={() => dispatch({type: 'login', payload: 'Maria'})}>Login</button>
                     <button className="btn"
-                    onClick={() => dispatch({type: 'numberAdd2'})}>+2</button> 
+                        onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button> 
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberMulti7' })}>*7</button> 
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button> 
+                     <button className="btn"
+                        onClick={() => dispatch({ type: 'numberInt' })}>Int</button> 
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberAddN', payload: -9 })}>-9</button> 
+                       <button className="btn"
+                        onClick={() => dispatch({type: 'numberAddN', payload: 11})}>+11</button> 
                 </div>
             </div>
         </div>
