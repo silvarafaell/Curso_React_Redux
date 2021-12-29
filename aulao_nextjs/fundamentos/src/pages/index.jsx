249d4d5400eff626f1e090b803d74cd4128c6b35
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-12-29 13:37:26 -0300

    Entendendo o JSX

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Inicio() {
-    return "Inicio"
+    return (
+        <div>
+            <h1>Fundamentos de Next.js e React</h1>
+            <h2>Vamos estudar esse framework</h2>
+        </div>
+    )
 }
