29cbaaf26213e496f594a6d801f88e62c1d3d1f7
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-12-31 09:55:26 -0300

    aula recomendacao de video

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -13,8 +13,8 @@ export default function Inicio() {
             <Navegador texto='Exemplo' destino='/exemplo' cor='crimson' />
             <Navegador texto='Navegacao #01' destino='/navegacao' cor='green' />  {/*podemos criar uma pasta com arquivo index dentro ou somente um arquivo(por exemplo o arquivo exemplo) */}
             <Navegador texto='Navegacap #02' destino='/cliente/123' cor='blue' />
-            <Navegador texto='Componente com Estado' destino='/estado' cor='pink' />
-            <Navegador texto='Integração com API #01' destino='/integracao_1' cor='yellow' />
+            <Navegador texto='Componente com Estado' destino='/estado' cor='#a45b71' />
+            <Navegador texto='Integração com API #01' destino='/integracao_1' cor='#42a9a9' />
         </div>
     )
 }
