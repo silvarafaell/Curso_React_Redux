90dcb9c91f11f862fa65accdaf3072c2fe6d5710
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2022-01-03 14:58:48 -0300

    Componente Tabela #01

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+    const clientes = [
+        new Cliente('Ana', 34, '1'),
+        new Cliente('Bia', 25, '2'),
+        new Cliente('Carlos', 29, '3'),
+        new Cliente('Gabi', 27, '1'),
+        new Cliente('Rafael', 27, '4'),
+    ]
+
   return (
       <div className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
       `}>
-          <Layout titulo="Cadastro Simples">
-            <span>Conteudo</span>  
-          </Layout>
+        <Layout titulo="Cadastro Simples">
+           <Tabela clientes={clientes}></Tabela>
+        </Layout>
     </div>
   )
 }
