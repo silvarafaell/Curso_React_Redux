8788d546b4994c80330029b3e0d84982ca5d5e88
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2022-01-04 13:34:38 -0300

    Alternando entre Tabela e Formulario

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -3,41 +3,66 @@ import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import Formulario from "../components/Formulario";
+import { useState } from "react";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 25, "2"),
+    new Cliente("Carlos", 29, "3"),
+    new Cliente("Gabi", 27, "1"),
+    new Cliente("Rafael", 27, "4"),
+  ];
 
-    const clientes = [
-        new Cliente('Ana', 34, '1'),
-        new Cliente('Bia', 25, '2'),
-        new Cliente('Carlos', 29, '3'),
-        new Cliente('Gabi', 27, '1'),
-        new Cliente('Rafael', 27, '4'),
-    ]
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
 
-    function clienteSelecionado(cliente: Cliente) {
-        console.log(cliente.nome)
-    }
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluido... ${cliente.nome}`);
+  }
 
-     function clienteExcluido(cliente: Cliente) {
-        console.log(`Excluido... ${cliente.nome}`)
-    }
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
 
   return (
-      <div className={`
+    <div
+      className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-      `}>
-          <Layout titulo="Cadastro Simples">
-               <div className="flex justify-end"> {/*justify-end coloca o botao no lado direito */}
-                <Botao cor="green" className="mb-4">Novo Cliente</Botao>
-              </div>
-            {/* <Tabela clientes={clientes}
-               clienteSelecionado={clienteSelecionado}
-               clienteExcluido={clienteExcluido}
-            /> */}
-              <Formulario cliente={clientes[0]}></Formulario>
-        </Layout>
+      `}
+    >
+      <Layout titulo="Cadastro Simples">
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              {/*justify-end coloca o botao no lado direito */}
+              <Botao
+                cor="green"
+                className="mb-4"
+                onClick={() => setVisivel("form")}
+              >
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={clientes[0]}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel("tabela")}
+          />
+        )}
+      </Layout>
     </div>
-  )
+  );
 }
