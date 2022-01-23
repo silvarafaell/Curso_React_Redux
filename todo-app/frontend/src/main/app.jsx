76c879c466ef197813149e374481c56af960214d
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2022-01-22 22:28:23 -0300

    Componentes Todo e About - exibindo ambos

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -2,10 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 // função
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 )
